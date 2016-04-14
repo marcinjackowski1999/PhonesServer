6605a6b4074f1292b182ab111b46e8aca415ca80
Marcin Jackowski <marcin.jackowski@droidsonroids.pl>
2016-04-14 20:53:19 +0200

    Fix silent push logic

--- phonesserver/routes/silentping.js
+++ phonesserver/routes/silentping.js
@@ -4,13 +4,13 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var Phone = require('../models/Phone.js');
 
-router.get('/:id', function(req, res, next) {
+router.put('/:id', function(req, res, next) {
   Phone.findById(req.params.id, function (err, post) {
     if (err) return next(err);
 
     var agent = require('../apn/header.js')
     var device = post.deviceToken;
-    
+
     agent.createMessage()
       .device(device)
       .contentAvailable(1)
